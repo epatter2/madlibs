29ffa3bd8353f1c698531777f5a2ae8d85d6b1f0
Ethan Patterson <epatter2@ggc.edu>
2016-05-08 02:47:57 +0000

    update 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  root                     'static_pages#home'
-  get   'help'      =>     'static_pages#help'
-  get   'about'     =>     'static_pages#about'
-  get   'contact'   =>     'static_pages#contact'
-  get   'signup'    =>     'users#new'
-  get   'input1'    =>     'static_pages#input1'
-  get   'story1'    =>     'static_pages#story1'
-  get    'login'    =>     'sessions#new'
-  post   'login'    =>     'sessions#create'
-  delete 'logout'   =>     'sessions#destroy'
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get   'input1'   => 'static_pages#input1'
+  get   'story1'   => 'static_pages#story1'
+  get    'signup'    => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Ethan Patterson",
+             email: "epatter2@ggc.edu",
+             password:              "ep1234",
+             password_confirmation: "ep1234",
              admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "user-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
                email: email,
